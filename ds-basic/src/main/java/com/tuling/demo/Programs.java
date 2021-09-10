107968d8e59329062e74ab66736a26f972b67b27
fujc <fjc.dane@gmail.com>
2021-09-10 14:27:16 +0800

    Jvm

---
+++ ds-basic/src/main/java/com/tuling/demo/Programs.java
@@ -0,0 +1,28 @@
+package com.tuling.demo;
+
+/**
+ * 判断一个数是否是2的n2次方，比如：2、4、8、16，而6、10不是。
+ *
+ * <p>
+ * &：二位余运算
+ * </p>
+ *
+ * @author : fjc.dane@gmail.com
+ * @createtime : 2021/9/10 11:06
+ */
+public class Programs {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * @param n
+     */
+    private void doCalc(int n) {
+
+    }
+}
---
+++ ds-btree/src/test/java/com/tuling/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuling.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
