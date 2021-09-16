e3717185279a9da86e7d47f2b274cf029b77ae76
fujc <fjc.dane@gmail.com>
2021-09-16 19:49:56 +0800

    HashMap

--- base-hashmap/src/com/tuling/HashMapTest.java
+++ base-hashmap/src/com/tuling/HashMapTest.java
@@ -1,9 +1,8 @@
 package com.tuling;
 
 import java.security.PublicKey;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * @author : fjc.dane@gmail.com
@@ -21,9 +20,29 @@ public class HashMapTest {
         //插入效率高：
         //访问效率低：
         HashMap<String, String> maps = new HashMap<>();
-        String result = maps.put("1", "LiLei");   // key ---> key.hashcode()---->
-        System.out.println(result);
-        maps.get("1");
+        maps.put("1", "1");   // key ---> key.hashcode()---->
+        maps.put("2", "2");
+
+        //ConcurrentModificationException
+        for (String key : maps.keySet()) {
+            if ("2".equals(key)) {
+                maps.remove(key);
+            }
+        }
+        //解决方案1：副本删除
+        for (Map.Entry<String, String> key : maps.entrySet()) {
+            if ("2".equals(key)) {
+                maps.remove(key);
+            }
+        }
+        //解决方案2：迭代器删除
+        Iterator iterator = maps.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = String.valueOf(iterator.next());
+            if ("2".equals(key)) {
+                iterator.remove();
+            }
+        }
 
         int m = 0;
         int n = 0;
