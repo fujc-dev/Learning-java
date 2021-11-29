5284340d0c831ae196cf721301e6d0adde3d46a6
fujc <fjc.dane@gmail.com>
2021-11-29 16:54:00 +0800

    深入浅出java多线程

--- multi-java/src/com/zc58s/Main.java
+++ multi-java/src/com/zc58s/Main.java
@@ -1,5 +1,8 @@
 package com.zc58s;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  *
  */
@@ -7,6 +10,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //ThreadPoolExecutor
+        //ScheduledThreadPoolExecutor
 
+        //这里会传递一个核心的线程个数，声明正式工5个
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(5);
+        //executor.schedule();
+        // executor.scheduleWithFixedDelay();
+        //scheduleAtFixedRate
+        //executor.scheduleAtFixedRate();
     }
 }
